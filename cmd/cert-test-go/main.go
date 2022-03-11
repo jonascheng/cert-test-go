9c47985d7ba49125c234af64a7e869d8b668647f
Jonas Cheng <jonas_cheng@trendmicro.com>
2022-03-11 16:57:20 +0800

    feat: check cert file

--- cmd/cert-test-go/main.go
+++ cmd/cert-test-go/main.go
@@ -2,12 +2,14 @@ package main
 
 import (
 	"bytes"
+	"crypto/tls"
 	"crypto/x509"
 	"encoding/pem"
 	"errors"
 	"fmt"
 	"log"
 	"os"
+	"time"
 )
 
 func check(e error) {
@@ -39,13 +41,42 @@ func decryptKey(key []byte, password string) ([]byte, error) {
 	return buf.Bytes(), nil
 }
 
-func main() {
+func loadCertificateByte(b []byte) (*x509.Certificate, error) {
+	block, _ := pem.Decode(b)
+	if block == nil {
+		return nil, errors.New("failed to decode certificate")
+	}
+	return x509.ParseCertificate(block.Bytes)
+}
+
+func readKey(keyFileName string) []byte {
 	// read key file
-	keyFile, err := os.ReadFile("server.key")
+	keyFile, err := os.ReadFile(keyFileName)
 	check(err)
 	log.Println(string(keyFile))
 	// decrypt key file
 	key, err := decryptKey(keyFile, "mypassword")
 	check(err)
 	log.Println(string(key))
+	return key
+}
+
+func readCert(certFileName string) []byte {
+	// read cert file
+	certFile, err := os.ReadFile(certFileName)
+	check(err)
+	log.Println(string(certFile))
+	certInfo, err := loadCertificateByte(certFile)
+	check(err)
+	if certInfo.NotAfter.Unix() < time.Now().Unix() {
+		log.Println("invalid certificate")
+	}
+	return certFile
+}
+
+func main() {
+	key := readKey("server.key")
+	cert := readCert("server.crt")
+	_, err := tls.X509KeyPair(cert, key)
+	check(err)
 }
